46bebd00f76253c81c4ee3c8a6a8687719e4c03f
lautaro <lautaro25@outlook.com.ar>
2026-05-26 00:04:52 -0300

    Improved JobParamsResponseSerializer

--- backend/agent/serializers/output_serializers.py
+++ backend/agent/serializers/output_serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 
+from jobs.choices import EmploymentTypes, WorkModeChoices
+
 class JobParamsResponseSerializer(serializers.Serializer):
-    keywords = serializers.CharField(read_only=True, allow_null=True)
-    location = serializers.CharField(read_only=True, allow_null=True)
-    employment_type =  serializers.BooleanField(allow_null=True)
-    min_salary = serializers.IntegerField(min_value=0, allow_null=True)
+    technologies = serializers.ListField(child=serializers.CharField(), required=False, allow_empty=True,default=[])
+    location = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    employment_type = serializers.ChoiceField(choices=EmploymentTypes.choices, required=False, allow_null=True)
+    min_salary = serializers.IntegerField(min_value=0, required=False, allow_null=True)
+    work_mode = serializers.ChoiceField(choices=WorkModeChoices.choices, required=False, allow_null=True)
