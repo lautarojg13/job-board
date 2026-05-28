c546e9cdd318bd8a332199861024596b0e55816f
lautaro <lautaro25@outlook.com.ar>
2026-05-28 14:14:36 -0300

    Created filters in JobPostQuerySet

--- backend/jobs/querysets.py
+++ backend/jobs/querysets.py
@@ -4,3 +4,12 @@ from .choices import JobPostStatus
 class JobPostQuerySet(models.QuerySet):
     def visible(self):
         return self.exclude(status=JobPostStatus.ARCHIVED)
+
+    def filter_by_location(self, location):
+        return self.filter(location__icontains=location)
+    
+    def filter_by_employment_type(self, employment_type):
+        return self.filter(employment_type__icontains=employment_type)
+    
+    def filter_by_min_salary(self, min_salary):
+        return self.filter(min_salary__gte=min_salary)
