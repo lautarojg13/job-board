15fc3d61a26383d95417ee7cd74bcf485cd82530
lautaro <lautaro25@outlook.com.ar>
2026-05-30 12:49:44 -0300

    refactor(companies): update factories for CompanyMember creation

--- backend/companies/factories.py
+++ backend/companies/factories.py
@@ -2,8 +2,8 @@ import factory
 
 from factory.django import DjangoModelFactory
 
-from companies.models import Company
-
+from companies.models import Company, CompanyMember
+from companies.choices import CompanyRoleChoices
 from users.factories import CustomUserFactory
 
 
@@ -13,4 +13,22 @@ class CompanyFactory(DjangoModelFactory):
         skip_postgeneration_save = True
     
     name = factory.Sequence(lambda n: f'Company {n}')
-    owner = factory.SubFactory(CustomUserFactory)
+
+    @factory.post_generation
+    def set_owner(obj, create, extracted, **kwargs):
+        if create:
+            owner = extracted or CustomUserFactory()
+            CompanyMember.objects.create(
+                company=obj,
+                user=owner,
+                company_role=CompanyRoleChoices.OWNER
+            )
+
+
+class CompanyMemberFactory(DjangoModelFactory):
+    class Meta:
+        model = CompanyMember
+
+    company = factory.SubFactory(CompanyFactory)
+    user = factory.SubFactory(CustomUserFactory)
+    company_role = CompanyRoleChoices.RECRUITER
