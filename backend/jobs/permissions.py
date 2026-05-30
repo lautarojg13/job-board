9da5f20d4ca716b7d0652742f7b0b43d775495bf
lautaro <lautaro25@outlook.com.ar>
2026-05-30 12:54:04 -0300

    Modified the IsJobOwner permission

--- backend/jobs/permissions.py
+++ backend/jobs/permissions.py
@@ -1,10 +1,17 @@
 from rest_framework.permissions import BasePermission
-
+from companies.models import CompanyMember
+from companies.choices import CompanyRoleChoices
 from .models import JobPost
+
 
 class IsJobOwner(BasePermission):
     message = "You are not allowed to perform this action"
 
     def has_object_permission(self, request, view, obj):
-    
-        return obj.posted_by == request.user or obj.company.owner == request.user
+        if obj.posted_by == request.user:
+            return True
+        return CompanyMember.objects.filter(
+            company=obj.company,
+            user=request.user,
+            company_role=CompanyRoleChoices.OWNER
+        ).exists()
