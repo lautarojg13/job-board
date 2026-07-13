f100fc4a77ecae89d91d869b954b4ea2c3bd2b63
lautaro <lautaro25@outlook.com.ar>
2026-07-13 19:36:31 -0300

    Fixed some env errors

--- backend/jobboard/settings.py
+++ backend/jobboard/settings.py
@@ -189,9 +189,9 @@ CELERY_TASK_SERIALIZER = 'json'
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = getattr(config, "EMAIL_HOST")
-EMAIL_PORT = getattr(config, "EMAIL_PORT")
-EMAIL_USE_TLS = getattr(config, "EMAIL_USE_TLS")
-EMAIL_HOST_USER = getattr(config, "EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = getattr(config, "EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = getattr(config, "DEFAULT_FROM_EMAIL")
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
