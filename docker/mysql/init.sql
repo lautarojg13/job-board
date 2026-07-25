60ccc01123d52273d70e41536bfacbdb7e941e07
lautaro <lautaro25@outlook.com.ar>
2026-07-25 16:55:16 -0300

    Removed useless system tools and modified db permissions

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,5 +1,3 @@
-GRANT ALL PRIVILEGES ON jobboard_db.* TO 'jobboard_user'@'%';
-
-GRANT CREATE, DROP ON *.* TO 'jobboard_user'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'jobboard_user'@'%';
 
 FLUSH PRIVILEGES;
