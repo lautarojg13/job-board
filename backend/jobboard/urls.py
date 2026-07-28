f58f4dcd00df80431018037c7e6c85733a79af08
lautaro <lautaro25@outlook.com.ar>
2026-07-27 21:35:10 -0300

    Added swagger documentation

--- backend/jobboard/urls.py
+++ backend/jobboard/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/", include("users.urls")),
@@ -11,4 +17,8 @@ urlpatterns = [
     
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
     path('auth/', include('dj_rest_auth.urls')),
+    
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
